cff8bab18c638c8a5fd741cb75aefcdbdb85d368
xingdabao <xy15532218813>
2020-10-24 21:19:30 +0800

    task

---
+++ precache-manifest.3fc946fd4b13135a4488bd9cd639a51b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c853074831d9914063dc84717548c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f4ea7f02a1cd660fecc",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "31f134c1297ea388214f",
+    "url": "/static/css/main.6924ece0.chunk.css"
+  },
+  {
+    "revision": "9f4ea7f02a1cd660fecc",
+    "url": "/static/js/2.bc700151.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.bc700151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f134c1297ea388214f",
+    "url": "/static/js/main.6dc87b73.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "a5de1d285829df698bc10f6c707aa5d9",
+    "url": "/static/media/list1.a5de1d28.jpg"
+  }
+]);
